060d7f391bb876eb2ee1be8299289aee6d6aa780
Durvesh Bodkar <durveshbodkar@gmail.com>
2022-06-16 23:01:47 +0530

    harvest front end completed

--- app/src/main/java/com/ynot/farmhelp/Harvest.java
+++ app/src/main/java/com/ynot/farmhelp/Harvest.java
@@ -14,8 +14,8 @@ import java.util.Calendar;
 
 public class Harvest extends AppCompatActivity {
 
-    TextView harvestDate, expectedDate;
-    ImageView calendarImage, calendarImage2;
+    TextView harvestDate, expectedDate, cropName, monthDemand, nextMonthDemand, remainingDemand;
+    ImageView calendarImage, calendarImage2, tomato, onion, potato, cropImage;
     DatePickerDialog.OnDateSetListener setListener;
 
     @Override
@@ -27,12 +27,21 @@ public class Harvest extends AppCompatActivity {
         expectedDate = findViewById(R.id.expectedDate);
         calendarImage = findViewById(R.id.calendarImage);
         calendarImage2 = findViewById(R.id.calendarImage2);
+        tomato = findViewById(R.id.tomato);
+        onion = findViewById(R.id.onion);
+        potato = findViewById(R.id.potato);
+        cropImage = findViewById(R.id.cropImage);
+        cropName = findViewById(R.id.cropName);
+        monthDemand = findViewById(R.id.monthDemand);
+        nextMonthDemand = findViewById(R.id.nextMonthDemand);
+        remainingDemand = findViewById(R.id.remainingDemand);
 
         Calendar calendar = Calendar.getInstance();
         final int year = calendar.get(Calendar.YEAR);
         final int month = calendar.get(Calendar.MONTH);
         final int day = calendar.get(Calendar.DAY_OF_MONTH);
 
+        int images[] = {R.drawable.tomato, R.drawable.onion, R.drawable.potato};
 
 
         harvestDate.setOnClickListener(new View.OnClickListener() {
@@ -100,6 +109,42 @@ public class Harvest extends AppCompatActivity {
                     }
                 }, year, month, day);
                 datePickerDialog.show();
+            }
+        });
+
+        tomato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                cropImage.setImageResource(images[0]);
+                cropName.setText("Tomato");
+                monthDemand.setText("50891/Qtls");
+                nextMonthDemand.setText("42561/Qtls");
+                remainingDemand.setText("8330/Qtls");
+            }
+        });
+
+        onion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                cropImage.setImageResource(images[1]);
+                cropName.setText("Onion");
+                monthDemand.setText("105754/Qtls");
+                nextMonthDemand.setText("55780/Qtls");
+                remainingDemand.setText("12543/Qtls");
+            }
+        });
+
+        potato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                cropImage.setImageResource(images[2]);
+                cropName.setText("Potato");
+                monthDemand.setText("99540/Qtls");
+                nextMonthDemand.setText("48560/Qtls");
+                remainingDemand.setText("10642/Qtls");
             }
         });
     }
