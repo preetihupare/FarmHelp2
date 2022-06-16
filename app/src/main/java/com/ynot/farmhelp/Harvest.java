39afc3cd4c080d89cb7d11ba36392861c4230aa3
Durvesh Bodkar <durveshbodkar@gmail.com>
2022-06-16 19:58:07 +0530

    harvest front end completed

--- app/src/main/java/com/ynot/farmhelp/Harvest.java
+++ app/src/main/java/com/ynot/farmhelp/Harvest.java
@@ -1,13 +1,107 @@
 package com.ynot.farmhelp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class Harvest extends AppCompatActivity {
+
+    TextView harvestDate, expectedDate;
+    ImageView calendarImage, calendarImage2;
+    DatePickerDialog.OnDateSetListener setListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_harvest);
+
+        harvestDate = findViewById(R.id.harvestDate);
+        expectedDate = findViewById(R.id.expectedDate);
+        calendarImage = findViewById(R.id.calendarImage);
+        calendarImage2 = findViewById(R.id.calendarImage2);
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+
+        harvestDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(Harvest.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        month = month + 1;
+                        String date = day + "/" + month + "/" + year;
+                        harvestDate.setText(date);
+
+
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+        calendarImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(Harvest.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        month = month + 1;
+                        String date = day + "/" + month + "/" + year;
+                        harvestDate.setText(date);
+
+
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+        expectedDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(Harvest.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        month = month + 1;
+                        String date = day + "/" + month + "/" + year;
+                        expectedDate.setText(date);
+
+
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+        calendarImage2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(Harvest.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        month = month + 1;
+                        String date = day + "/" + month + "/" + year;
+                        expectedDate.setText(date);
+
+
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
     }
+
 }
